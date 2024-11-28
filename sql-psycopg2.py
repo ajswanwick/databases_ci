182e2ec13e5e197d5e10c663a7ab6c3fdc195e33
Adam Swanwick <a.j.swanwick@gmail.com>
2024-11-28 12:47:33 +0000

    update

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -3,19 +3,24 @@ import psycopg2
 
 #connect to chinook database
 
-connection = psycopg2.connect(database = "chinook")
+connection = psycopg2.connect(database ="chinook")
 
 #Build a Cursor object of the database
 cursor = connection.cursor()
 
 #Query1 - select all records from the "Artist" table
-cursor.execute('SELECT * FROM "Artist"')
+#cursor.execute('SELECT * FROM "Artist"')
+
+#Query2 - select only queen from the artist table 
+#cursor.execute('SELECT * FROM "Artist" WHERE "Name"= %s', ["Queen"])
+
+cursor.execute('SELECT * FROM "Artist" WHERE "Name" =%s', ["Metallica"])
 
 #fetch the results (multiple)
-results = cursor.fetchall()
+#results = cursor.fetchall()
 
 #fetch results(single)
-#results = cursor.fetchone()
+results = cursor.fetchone()
 
 #close the connection
 connection.close()
